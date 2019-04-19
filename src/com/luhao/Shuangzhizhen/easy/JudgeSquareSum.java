2f6db7df3fc910b940571dd731bf80624d21e7ae
陆浩 <826643679@qq.com>
2019-04-19 20:58:28 +0800

    633 判断完全平方数

---
+++ src/com/luhao/Shuangzhizhen/easy/JudgeSquareSum.java
@@ -0,0 +1,29 @@
+package com.luhao.Shuangzhizhen.easy;
+
+/**
+ * 633
+ * @author Administrator
+ *
+ */
+public class JudgeSquareSum {
+
+	public static void main(String[] args) {
+		judgeSquareSum(5);
+	}
+	
+    public static boolean judgeSquareSum(int c) {
+		int i = 0, j = (int) Math.sqrt(c);
+		while(i<=j){
+			int powsum = i*i+j*j;
+			if(powsum==c){
+				return true;
+			}
+			else if(powsum<c){
+				i++;
+			}else{
+				j--;
+			}
+		}
+    	return false;
+    }
+}
