a5c26cc24290b6cc3043c94fe8b37246c74287eb
陆浩 <826643679@qq.com>
2019-04-25 08:03:08 +0800

    453. 最小移动次数使数组元素相等

---
+++ src/com/luhao/Tanxin/MinMoves.java
@@ -0,0 +1,30 @@
+package com.luhao.Tanxin;
+
+import java.util.Arrays;
+
+/**
+ * 453. 最小移动次数使数组元素相等
+ * @author Administrator
+ *
+ */
+
+public class MinMoves {
+	//n-1个数同时加一，就好比每次有一个数自身减一
+	//数组最后的数为最小值
+	//每个元素减去最小值，求和
+	public static int minMoves(int[] nums) {
+		Arrays.sort(nums);
+		int res = 0;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			nums[i] = nums[i] - nums[0];
+			res = res + nums[i];
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] num = { 1, 2, 3 };
+		minMoves(num);
+	}
+
+}
